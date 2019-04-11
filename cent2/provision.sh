379cf779e76232aacdda21ee1bb25105d068f6f3
Maksim Murygin <mail@muryginm.com>
2019-04-11 07:53:20 +0700

    add dns to sandbox instances

--- cent2/provision.sh
+++ cent2/provision.sh
@@ -5,5 +5,9 @@ echo "export LC_ALL=en_US.utf-8" >> /etc/profile.d/locale.sh
 echo "export LANG=en_US.utf-8" >> /etc/profile.d/locale.sh
 mandb
 
-yum install -y vim wget
+yum install -y vim
+
+echo "10.0.10.2 s1.home.local" >> /etc/hosts
+echo "10.0.10.3 s2.home.local" >> /etc/hosts
+echo "10.0.10.4 s3.home.local" >> /etc/hosts
 
